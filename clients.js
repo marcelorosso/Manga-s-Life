bfcbf5c580a9e6b5fd9e330dc21f14cd2078a893
Marcelo Rosso <95058839+marcelorosso@users.noreply.github.com>
2024-02-17 12:25:04 -0300

    Update clients.js

--- clients.js
+++ clients.js
@@ -1,3 +1,57 @@
+//Array de Objetos - Creación de Registro de Usuarios (push on web log to interact with it - login.html)
+
+/* Se procedió a crear una clase de usuarios registrados en la web, con el proposito de a través de una
+function de agregado de usuarios, ir incorporando nuevos users al array userList, el cual se activa al 
+presionar el logo de la página en la web de login. Con el buttom submit se vuelve al home o index.*/
+
+class Users {
+    constructor (personalName, address, location, mail, number,) {
+        this.id = Date.now();
+        this.name = personalName.toUpperCase();
+        this.address = address.toUpperCase();
+        this.city = location.toUpperCase();
+        this.mail = mail.toUpperCase();
+        this.cellphoneNumber = parseFloat (number);
+    }
+}
+
+const userList = [];
+userList.push (new Users ("marcelo rosso", "publica 1 norte 6920 - universitario", "cordoba", "rossomarcelogabriel@gmail.com", 3517911990));
+userList.push (new Users ("celeste massara", "publica 1 norte 6920 - universitario", "cordoba", "celemassara@gmail.com", 3513140020));
+
+function addUser () {
+    let nom = prompt ("Introduce the name and surname:");
+    let ad = prompt ("Introduce the address:");
+    let loc = prompt ("Introduce the city:");
+    let em = prompt ("Introduce the email:");
+    let cell = prompt ("Introduce the phone number:");
+    userList.push(new Users(nom, ad, loc, em, cell));
+    swal.fire ({
+        text: "Name and Surname: "  + nom + " " + "Address: " + ad + " " + "City: " + loc + " " + "E-mail: " + em + " " + "Cellphone Number: " + cell,
+        color: "green",
+        timer: 3500,
+        showConfirmButton: false
+    })
+    // alert ("Name and Surname: " + nom);
+    // alert ("Address: " + ad);
+    // alert ("City: " + loc);
+    // alert ("E-mail: " + em);
+    // alert ("Cellphone Number: " + cell);
+    console.table (userList); 
+}
+
+
+//EVENTOS PARA INGRESO DE USUARIOS Y CONTRASEÑAS EN LOGIN PAGE
+
+/* Se procedió a agregar simples eventos dentro de la página de login al sistema como usuario
+registrado. 
+
+- Se utilizó una clase creada para pequeñas modificaciones al hacer foco y salir del foco en los inputs
+- Un control de escritura simple
+- Un aviso de acuerdo a futuros desarrollos con relación a los registros del login
+*/
+
+// Variables
 
 const userName = document.querySelector("#userName");
 const userEmail = document.querySelector("#userEmail");
